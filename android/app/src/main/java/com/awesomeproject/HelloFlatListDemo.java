cdd0725e0c46ee9e283b2690f440b87c294a93d3
wangxianyou <303681285@qq.com>
2018-09-07 17:04:09 +0800

    到flatlist

---
+++ android/app/src/main/java/com/awesomeproject/HelloFlatListDemo.java
@@ -0,0 +1,13 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactActivity;
+
+import javax.annotation.Nullable;
+
+public class HelloFlatListDemo extends ReactActivity{
+    @Nullable
+    @Override
+    protected String getMainComponentName() {
+        return "HelloFlatList";
+    }
+}
---
+++ android/app/src/main/java/com/awesomeproject/HelloScrollViewDemo.java
@@ -0,0 +1,13 @@
+package com.awesomeproject;
+
+import com.facebook.react.ReactActivity;
+
+import javax.annotation.Nullable;
+
+public class HelloScrollViewDemo extends ReactActivity {
+    @Nullable
+    @Override
+    protected String getMainComponentName() {
+        return "HelloScrollView";
+    }
+}
