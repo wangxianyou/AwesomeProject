567c868e488bce3e80242ecac4ee7e8948ea9d3e
wangxianyou <303681285@qq.com>
2018-10-18 16:44:44 +0800

    网络

--- js/HelloFlatList.js
+++ js/HelloFlatList.js
@@ -1,35 +1,59 @@
 import React, {Component} from 'react'
-import {FlatList, Text,StyleSheet,View} from 'react-native'
+import {FlatList, Text, StyleSheet, View, ActivityIndicator} from 'react-native'
 
 export default class HelloFlatList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {isLoading: true}
+    }
+
+    componentDidMount() {
+        return fetch("https://facebook.github.io/react-native/movies.json")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSourse: responseJson.movies
+                })
+            }).catch((e) => {
+                console.log(e)
+            });
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loadingstyle}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
+        }
+
         return (
             <View style={{flex: 1}}>
                 <FlatList
-                    data={[
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                        {key: "haha"},
-                    ]}
-                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                    data={this.state.dataSourse}
+                    renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
+                    keyExtractor = {(item,index) => item.id}
                 />
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
-    item:{
-        fontSize:16,
-        color:'red',
+    item: {
+        fontSize: 16,
+        color: 'red',
+        flex: 1,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        height: 44
+    },
+
+    loadingstyle:{
         flex:1,
-        textAlign:'center',
-        textAlignVertical:'center',
-        height:44
+        justifyContent:'center',
+        alignItems:'center',
+        padding:8
     }
 });
